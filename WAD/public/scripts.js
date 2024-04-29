449693860ab87eee1b549bf196f74ad50d1d3aac
Alaeddine Jmila <jmila.aladin@gmail.com>
2024-04-29 14:22:20 +0100

    Add and remove items from basket

--- WAD/public/scripts.js
+++ WAD/public/scripts.js
@@ -1,29 +1,62 @@
-function updateCartCount(clearCount) {
-  const cartCountElement = document.getElementById('cart-items-count')
+function updateBasketCount(currentCount) {
+  const basketCountElement = document.getElementById('basket-items-count')
   const plantElement = document.querySelector('.fa-pagelines')
-  let currentCount = parseInt(window.localStorage.getItem('cartCount'))
 
-  if (clearCount) {
-    cartCountElement.innerText = ''
+  if (!currentCount) {
+    basketCountElement.innerText = ''
     plantElement.classList.remove('show')
-    window.localStorage.removeItem('cartCount')
+    window.localStorage.removeItem('basketCount')
     return
   }
 
-  if (!currentCount) {
-    currentCount = 1
-    cartCountElement.innerText = currentCount
-    window.localStorage.setItem('cartCount', currentCount)
-  } else {
-    currentCount += 1
-    cartCountElement.innerText = currentCount
-    window.localStorage.setItem('cartCount', currentCount)
-  }
+  basketCountElement.innerText = currentCount
+  window.localStorage.setItem('basketCount', currentCount)
 
-  if (currentCount >= 10) cartCountElement.classList.add('two-digits')
-  else cartCountElement.classList.remove('two-digits')
+  if (currentCount >= 10) basketCountElement.classList.add('two-digits')
+  else basketCountElement.classList.remove('two-digits')
 
   plantElement.classList.add('show')
 }
 
-updateCartCount()
+function displayBasketCount() {
+  const basket = JSON.parse(window.localStorage.getItem('basket')) || []
+  const basketCountElement = document.getElementById('basket-items-count')
+  const plantElement = document.querySelector('.fa-pagelines')
+  const currentCount = basket.length
+  if (currentCount) {
+    basketCountElement.innerText = currentCount
+    plantElement.classList.add('show')
+    if (currentCount >= 10) basketCountElement.classList.add('two-digits')
+    else basketCountElement.classList.remove('two-digits')
+  }
+}
+
+const addToBasketButtons = document.querySelectorAll('.product-info .btn')
+addToBasketButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    addToBasket(button.parentNode.parentNode.dataset.id)
+  })
+})
+
+function addToBasket(productId) {
+  const basket = JSON.parse(window.localStorage.getItem('basket')) || []
+  basket.push(+productId)
+  updateBasketCount(basket.length)
+  window.localStorage.setItem('basket', JSON.stringify(basket))
+}
+
+function removeFromBasket(productId) {
+  const basket = JSON.parse(window.localStorage.getItem('basket')) || []
+  const index = basket.indexOf(productId)
+  console.log('index ', index)
+
+  if (index !== -1) {
+    console.log('before ', basket.length)
+    basket.splice(index, 1)
+    console.log('after ', basket.length)
+    updateBasketCount(basket.length)
+    window.localStorage.setItem('basket', JSON.stringify(basket))
+  }
+}
+
+window.addEventListener('load', displayBasketCount)
