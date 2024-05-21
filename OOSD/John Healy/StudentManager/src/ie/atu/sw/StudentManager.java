2ea942be427fd6a758d38e80b805039cde8deab2
Alaeddine Jmila <jmila.aladin@gmail.com>
2024-05-21 08:27:31 +0100

    implement add student and auto expand array

--- OOSD/John Healy/StudentManager/src/ie/atu/sw/StudentManager.java
+++ OOSD/John Healy/StudentManager/src/ie/atu/sw/StudentManager.java
@@ -14,9 +14,26 @@ public class StudentManager {
 		for (int i = 0; i < student.length; i++) {
 			if (student[i] == null) {
 				student[i] = s;
+				return;
 			}
 		}
 		
+		int index = getExpandedIndex();
+		student[index] = s;
+		
+	}
+	
+	private int getExpandedIndex() {
+		Student[] temp = new Student[student.length * 2];
+		
+		for(int i = 0; i < student.length; i++) {
+			temp[i] = student[i];
+		}
+		
+		int index = student.length;
+		student = temp;
+		
+		return index;
 	}
 	
 	public boolean delete(String sid) {
