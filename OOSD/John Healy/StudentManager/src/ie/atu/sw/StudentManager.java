3d1f22e6f9113603c47b757166c5145112211301
Alaeddine Jmila <jmila.aladin@gmail.com>
2024-05-25 13:37:29 +0100

    Complete and Package Student Manager app

--- OOSD/John Healy/StudentManager/src/ie/atu/sw/StudentManager.java
+++ OOSD/John Healy/StudentManager/src/ie/atu/sw/StudentManager.java
@@ -1,5 +1,22 @@
 package ie.atu.sw;
 
+import java.time.LocalDate;
+import java.util.concurrent.ThreadLocalRandom;
+
+// to package the java app, navigate into the bin directory of the project
+// then run the following command:
+// jar -cf nameOfFinalOutput.jar *
+// cf stands for Create File and Jar is the jar where we put the coffee beans
+
+// To run the deployed package, run the following:
+// For linux
+// java -cp .:./smanager.jar ie.atu.sw.Runner
+// cp stands for Class Path
+// For windows 
+// it's practically the same except the path syntax is different
+// To be safe just provide the full path to the package and if it contains spaces
+// just wrap it in double quotes
+
 public class StudentManager {
 	private static final int INITIAL_CAPACITY = 10;
 	private Student[] student = null;
@@ -8,6 +25,7 @@ public class StudentManager {
 	
 	public StudentManager() {
 		student = new Student[INITIAL_CAPACITY];
+		init();
 	}
 
 	public void add(Student s) {
@@ -84,5 +102,28 @@ public class StudentManager {
 			if(student[i] != null) total++;
 		}
 		return total;
+	}
+	
+	private void init() {
+		ThreadLocalRandom rand = ThreadLocalRandom.current();
+		
+		String[] fnames = {"Joe", "Jane", "Anne", "Pat"};
+		String[] surnames = {"Smith", "Murphy", "Burke", "O'Brien"};
+		Course[] courses = Course.values();
+		int max = 100_000;
+		long start = System.currentTimeMillis();
+		
+		for (int i = 0; i < max; i++) {
+			Student rs = new Student("G00" + i, 
+					fnames[rand.nextInt(0, fnames.length)], 
+					surnames[rand.nextInt(0, surnames.length)], 
+					LocalDate.now(),
+					new Address("Galway"),
+					courses[rand.nextInt(0, courses.length)]
+			);
+			add(rs);
+		}
+		
+		System.out.println("Time (ms): " + (System.currentTimeMillis() - start));
 	}
 }
