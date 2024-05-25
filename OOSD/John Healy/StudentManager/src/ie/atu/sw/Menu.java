b72dbde85679ab5e9e7f431b608fe80d48465617
Alaeddine Jmila <jmila.aladin@gmail.com>
2024-05-25 12:04:46 +0100

    Complete Menu implementation

--- OOSD/John Healy/StudentManager/src/ie/atu/sw/Menu.java
+++ OOSD/John Healy/StudentManager/src/ie/atu/sw/Menu.java
@@ -1,9 +1,12 @@
 package ie.atu.sw;
 
 import static java.lang.System.out;
+
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Menu {
+	private StudentManager sm = new StudentManager();
 	private Scanner s;
 	private boolean keepRunning = true;
 
@@ -31,22 +34,62 @@ public class Menu {
 
 	private void add() {
 		out.println("[Info] Add a Student");
+		
+		out.println("Enter Student ID>");
+		String sid = s.next();
+		
+		out.println("Enter Student Firstname>");
+		String fname = s.next();
+		
+		out.println("Enter Student Surname>");
+		String surname = s.next();
+		
+		// Press Ctrl + Space bar to pull the Student constructor
+		Student temp = new Student(sid, fname, surname,
+				LocalDate.now(), new Address("Galway"), Course.SOFT_DEV);
+		
+		sm.add(temp);
 	}
 
 	private void delete() {
 		out.println("[Info] Delete a Student");
+		out.println("Enter Student ID>");
+		String sid = s.next();
+		
+		if (sm.delete(sid)) {
+			out.println("[Info] Student " + sid + " deleted");
+		}else {
+			out.println("[Info] Cannot find Student " + sid);
+		}
 	}
 
 	private void findByID() {
 		out.println("[Info] Find Student by ID");
+		out.println("Enter Student ID>");
+		String sid = s.next();
+		Student student = sm.getStudentByID(sid);
+		
+		if (student != null) {
+			out.println(student);
+		} else {
+			out.println("[Info] Cannot find Student " + sid);
+		}
+			
 	}
 
 	private void findStudentsByFirstname() {
 		out.println("[Info] Find Student by Firstname");
+		out.println("Enter Student Firstname>");
+		String fname = s.next();
+		Student[] results = sm.getStudentsByFirstname(fname);
+		
+		for (Student student : results) {
+			out.println(student);
+		}
 	}
 
 	private void getTotal() {
-		out.println("[Info] Total Number of Students");
+		out.println("Total Number of Students: " + sm.size());
 	}
 
 	private void showOptions() {
