8c9cf6596db2969238fd3e7a2cd45db05fe78566
Alaeddine Jmila <jmila.aladin@gmail.com>
2024-06-05 07:35:07 +0100

    Add ConsoleLogger

---
+++ OOSD/John Healy/WordEmbedding/src/ie/atu/sw/ConsoleLogger.java
@@ -0,0 +1,32 @@
+package ie.atu.sw;
+
+import static java.lang.System.out;
+
+public class ConsoleLogger {
+
+	public void log(LogLevel level, String message) {
+		switch (level) {
+		case INFO -> logInfo(level, message);
+		case WARN -> logWarning(level, message);
+		case ERROR -> logError(level, message);
+		}
+	}
+
+	private void logInfo(LogLevel level, String message) {
+		out.print(ConsoleColour.GREEN_BOLD);
+		out.println(level.getMessage() + " " + message);
+		out.print(ConsoleColour.RESET);
+	}
+	
+	private void logWarning(LogLevel level, String message) {
+		out.print(ConsoleColour.YELLOW_BOLD);
+		out.println(level.getMessage() + " " + message);
+		out.print(ConsoleColour.RESET);
+	}
+	
+	private void logError(LogLevel level, String message) {
+		out.print(ConsoleColour.RED_BOLD);
+		out.println(level.getMessage() + " " + message);
+		out.print(ConsoleColour.RESET);
+	}
+}
--- OOSD/John Healy/WordEmbedding/src/ie/atu/sw/LogLevel.java
+++ OOSD/John Healy/WordEmbedding/src/ie/atu/sw/LogLevel.java
@@ -1,5 +1,18 @@
 package ie.atu.sw;
 
 public enum LogLevel {
+ INFO ("[INFO]"),
+ WARN ("[WARNING]"),
+ ERROR ("[ERROR]");
+ 
+ private final String message;
 
+ LogLevel(String message) {
+	 this.message = message;
+ }
+ 
+ public String getMessage() {
+	 return message;
+ }
+	
 }
