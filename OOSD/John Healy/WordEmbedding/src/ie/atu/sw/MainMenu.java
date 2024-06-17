03c308c8b131fe46b2b92cd4f8733bbe861cfcf3
Alaeddine Jmila <jmila.aladin@gmail.com>
2024-06-17 08:45:39 +0100

    Complete first round of dynamic length plotter

--- OOSD/John Healy/WordEmbedding/src/ie/atu/sw/MainMenu.java
+++ OOSD/John Healy/WordEmbedding/src/ie/atu/sw/MainMenu.java
@@ -131,7 +131,7 @@ public class MainMenu {
 		return result;
 	}
 	
-	private String[][] generateResultPhrases(String[] searchTerms, String[][][] searchResults) {
+	private String[][] generateResultPhrases(String[] searchTerms, String[][][] searchResults) throws IOException {
 		String[] noMatchResults = new String[]{"another", "an", "one", "the", "same", "is", 
 											"whose", "comes", "with", "on", "this", "as", "s",
 											"for", "first", "it", "which", "of", "turned", "but", 
@@ -164,12 +164,14 @@ public class MainMenu {
 			}
 		}
 		out.println(Arrays.toString(resultPhrases[0]));
-		dynamicPlotTemplate(resultPhrases);
+		String[] formats = dynamicPlotTemplate(resultPhrases);
+		generateOutputFile(resultPhrases, formats);
 		return resultPhrases;
 	}
 	
-	private void dynamicPlotTemplate(String[][] data) {
+	private String[] dynamicPlotTemplate(String[][] data) {
 		int longestResultLength = 0;
+		String[] formats = new String[5];
 		
 		for (String [] entry : data) {
 			if (entry[0].length() > longestResultLength) {
@@ -177,13 +179,37 @@ public class MainMenu {
 			}
 		}
 		
-		String resultRowFormat = "| %-" + longestResultLength + "s |";
-		out.println(resultRowFormat);
+		formats[0] = "| Result%-" + (longestResultLength - 6) + "s |  Score(%%)  |%n";
+		formats[1] = "| %-" + longestResultLength + "s |    %-5s   |%n";
+		formats[2] = "|-%-" + longestResultLength + "s-+----%-5s---|%n";
+		formats[3] = "--%-" + longestResultLength + "s------%-5s----%n";
+		formats[4] = "-".repeat(longestResultLength);
+		out.println(Arrays.toString(formats));
+		return formats;
+	}
+	
+	private void generateOutputFile(String[][] searchResults, String[] formats) throws IOException {
+		FileWriter out = new FileWriter("out.txt");
+		PrintWriter print = new PrintWriter(out);
+		
+		print.printf(formats[0], "");
+		print.printf(formats[2], formats[4], "-----");
+		for (int i = 0; i < searchResults.length; i++) {
+				print.printf(formats[1], searchResults[i][0], searchResults[i][1]);
+				if (i != searchResults.length - 1) {
+					print.printf(formats[2], formats[4], "-----");
+				} else {		
+					print.printf(formats[3], formats[4], "-----");
+				}
+		}
+		print.println();
+
+		print.close();
+
+		Runner.launchFile("out.txt");
 	}
 	
 	private void generateOutputFile(String[][][] searchResults) throws IOException {
-
-	
 		FileWriter out = new FileWriter("out.txt");
 		PrintWriter print = new PrintWriter(out);
 		
