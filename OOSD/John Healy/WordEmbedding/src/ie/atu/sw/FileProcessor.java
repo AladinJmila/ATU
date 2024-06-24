8ab3182d14f2a01b4c599388137932e7a684e7a6
Alaeddine Jmila <jmila.aladin@gmail.com>
2024-06-24 15:58:03 +0100

    Comment FileProcessor

--- OOSD/John Healy/WordEmbedding/src/ie/atu/sw/FileProcessor.java
+++ OOSD/John Healy/WordEmbedding/src/ie/atu/sw/FileProcessor.java
@@ -5,54 +5,73 @@ import static java.lang.System.out;
 import java.io.*;
 
 public class FileProcessor {
+	// Constants defining the number of words and features in the embeddings file
 	public static final int WORDS_COUNT = 59_602;
 	public static final int FEATURES_COUNT = 50;
+	// Flag indicating if the arrays have been created
 	private boolean createdArrays;
 	
+	// File path to the embeddings file
 	private String file;
+	// Array to store the words from the embeddings file
 	private String[] words = new String[WORDS_COUNT];
+	// 2D array to store the embeddings
 	private double[][] embeddings = new double[WORDS_COUNT][FEATURES_COUNT];
+	// Instance of ConsoleLogger to log messages
 	private ConsoleLogger cLogger;
 	
+	// Constructor initializing the file path and ConsoleLogger instance
 	FileProcessor(String filePath) {
 		file = filePath;
 		cLogger = new ConsoleLogger();
 	}
 	
+	// Generate arrays from the embeddings file
 	private void generateArrays() {
 		try {
+			// Initialize BufferedReader to read the file
 			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
 			String line = null;
 			int index = 0;
 			
+			// Log the start of file processing
 			cLogger.log(LogLevel.INFO, "Processing the input file");
 			
+			// Read each line from the file
 			while ((line = br.readLine()) != null) {
+				// Split the line by commas to separate words and embeddings
 				String[] items = line.split(",");
 				for (int i = 0; i < items.length; i++) {
 					if (i == 0) {
+						// The first item is the word
 						words[index] = items[i];
 					} else {
+						// The rest of the items are the embedding features
 						embeddings[index][i - 1] = Double.parseDouble(items[i]);
 					}
 					
 				}
 
+				// Stop reading if the maximum word count is reached
 				if (index++ >= WORDS_COUNT - 1) break;
 			}
 			
+			// Mark arrays as created
 			createdArrays = true;
 		} catch (Exception e) {
+			// Print any exceptions that occur during file processing
 			System.out.println(e);
 		}
 	}
 	
+	// Get the words array, generate it if not already created
 	public String[] getWordsArray() {
 		if (!createdArrays) generateArrays();
 		
 		return words;
 	}
 	
+	// Get the embeddings array, generate it if not already created
 	public double[][] getEmbeddingsArray() {
 		if (!createdArrays) generateArrays();
 		
