512c41fb49c749998436091fc972baac9d0d0609
Alaeddine Jmila <jmila.aladin@gmail.com>
2024-12-15 13:13:38 +0000

    Add menu

--- ASD/AgileSoftwareDevelopment/src/ie/atu/dip/Runner.java
+++ ASD/AgileSoftwareDevelopment/src/ie/atu/dip/Runner.java
@@ -1,8 +1,87 @@
 package ie.atu.dip;
 
+import static java.lang.System.out;
+import java.util.Scanner;
+
 public class Runner {
+	private boolean isFirstRun = true;
+	private boolean keepRunning = true;
+	private Scanner scanner;
+	private InputValidator validator;
+	
+	Runner() {
+		scanner = new Scanner(System.in);
+		validator = new InputValidator(scanner);
+	}
+	
+	public void init() {
+		while(keepRunning) {
+			showMenu();
+			
+			// Define the valid range for the menu choices
+			int[] range = {1, 9};
+			// Validate and get suer input for menu choice
+			int choice = validator.validateNumericInput(() -> showMenu(), range);
+			
+			// Handle then user's menu choice
+			switch(choice) {
+				case 1 -> out.println(choice);
+				case 2 -> out.println(choice);
+				case 3 -> out.println(choice);
+				case 4 -> out.println(choice);
+				case 5 -> out.println(choice);
+				case 6 -> out.println(choice);
+				case 7 -> out.println(choice);
+				case 8 -> out.println(choice);
+				case 9 -> keepRunning = false;
+				default -> out.println("Invalid Selection, choose a number from " + range[0] + " to " + range[1]+ ".");
+			}
+			
+			
+		}
+	}
+	
+	public void showMenu() {
+		
+
+		if (isFirstRun) {
+		out.println();
+		out.println("************************************************************");
+		out.print("*     ");
+		out.print("ATU - Dept. of Computer Science & Applied Physics");
+		out.println("    *");
+		out.println("*                                                          *");
+		out.print("*        ");
+		out.print("Banking Application Unit Testing Assignment");
+		out.println("       *");
+		out.println("*                                                          *");
+		out.println("************************************************************");
+		}
+
+	    System.out.println();
+        System.out.println("Welcome to our Banking Service:");
+        System.out.println("---------------------------------------------------");
+        System.out.println("| 1 | Add New Account");
+        System.out.println("| 2 | Deposit Money");
+        System.out.println("| 3 | Withdraw Money");
+        System.out.println("| 4 | Approve Loan");
+        System.out.println("| 5 | Repay Loan");
+        System.out.println("| 6 | Check Account Balance");
+        System.out.println("| 7 | Check Loan Amount");
+        System.out.println("| 8 | Check Total Deposits");
+        System.out.println("| 9 | Quit");
+        System.out.println("---------------------------------------------------");
+        System.out.print("Select Option [1-9]> ");
+		
+		isFirstRun = false;
+
+	}
+	
 	public static void main(String[] args) {
 	    // Create a new banking application instance
+		new Runner().init();
+		
+		/*
 	    BankingApp bank = new BankingApp();
 
 	    // Add accounts
@@ -27,6 +106,7 @@ public class Runner {
 
 	    // Check total deposits in the bank
 	    System.out.println("Total deposits in the bank: " + bank.getTotalDeposits());
+	    */
 	}
 
 }
