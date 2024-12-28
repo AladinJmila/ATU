11e933ad512bc71cf882cfb2642d3fb613328ac0
Alaeddine Jmila <jmila.aladin@gmail.com>
2024-12-28 12:57:50 +0000

    Extract OutputHandler

---
+++ SDDS/TextSimplifier/src/ie/atu/sw/OutputHandler.java
@@ -0,0 +1,64 @@
+package ie.atu.sw;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class OutputHandler {
+    private ConsoleLogger logger = new ConsoleLogger();
+    private String outputPath = "./";
+    private String fileName = generateFormattedDateTime() + " - simplified.txt";
+
+    OutputHandler() {
+    }
+
+    OutputHandler(String inputPath) {
+        this.fileName = generateFormattedDateTime() + " - " + extractFileName(inputPath) + " - simplified.txt";
+    }
+
+    OutputHandler(String outputPath, String inputPath) {
+        this.outputPath = outputPath;
+        this.fileName = generateFormattedDateTime() + " - " + extractFileName(inputPath) + " - simplified.txt";
+    }
+
+    public void generateOutputFile(List<String> linesToPlot) throws IOException {
+        var filePath = Paths.get(outputPath, fileName).toString();
+        var out = new FileWriter(filePath);
+        var print = new PrintWriter(out);
+
+        // Write each line to the output file
+        linesToPlot.forEach(line -> print.printf("%s%n", line));
+
+        print.println();
+
+        print.close();
+
+        logger.info("Results file will launch automatically!");
+
+        System.out.println(ConsoleColour.GREEN); // Change the colour of the console text
+        int size = 100; // The size of the meter. 100 equates to 100%
+        for (int i = 0; i < size; i++) { // The loop equates to a sequence of processing steps
+            Utilities.printProgress(i + 1, size); // After each (some) steps, update the progress meter
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } // Slows things down so the animation is visible
+        }
+        // Launch the output file
+        Utilities.launchFile(filePath);
+    }
+
+    private String extractFileName(String path) {
+        var pathParts = path.split("/");
+        var fileName = pathParts[pathParts.length - 1];
+        return fileName.split("\\.")[0];
+    }
+
+    private String generateFormattedDateTime() {
+        return LocalDateTime.now().toString().replace("T", " ").split("\\.")[0].replaceAll(":", "_");
+    }
+}
--- SDDS/TextSimplifier/src/ie/atu/sw/TextSimplifier.java
+++ SDDS/TextSimplifier/src/ie/atu/sw/TextSimplifier.java
@@ -1,13 +1,8 @@
 package ie.atu.sw;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentSkipListMap;
@@ -16,9 +11,9 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class TextSimplifier {
-    CosineDistance cosineDistance = new CosineDistance();
-    QuickSort quickSort = new QuickSort();
-    ConsoleLogger logger = new ConsoleLogger();
+    private CosineDistance cosineDistance = new CosineDistance();
+    private QuickSort quickSort = new QuickSort();
+    private ConsoleLogger logger = new ConsoleLogger();
     private ConcurrentHashMap<String, double[]> embeddingsMap;
     private ConcurrentHashMap<String, double[]> google1000Map;
     private String inputFilePath = "";
@@ -83,34 +78,7 @@ public class TextSimplifier {
             pool.shutdown();
             pool.awaitTermination(1, TimeUnit.MINUTES);
 
-            generateOutputFile(textResults.values().stream().toList());
+            new OutputHandler(inputFilePath).generateOutputFile(textResults.values().stream().toList());
         }
-    }
-
-    private void generateOutputFile(List<String> linesToPlot) throws IOException {
-        FileWriter out = new FileWriter("out.txt");
-        PrintWriter print = new PrintWriter(out);
-
-        // Write each line to the output file
-        linesToPlot.forEach(line -> print.printf("%s%n", line));
-
-        print.println();
-
-        print.close();
-
-        logger.info("Results file will launch automatically!");
-
-        System.out.println(ConsoleColour.GREEN); // Change the colour of the console text
-        int size = 100; // The size of the meter. 100 equates to 100%
-        for (int i = 0; i < size; i++) { // The loop equates to a sequence of processing steps
-            Utilities.printProgress(i + 1, size); // After each (some) steps, update the progress meter
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } // Slows things down so the animation is visible
-        }
-        // Launch the output file
-        Utilities.launchFile("out.txt");
     }
 }
